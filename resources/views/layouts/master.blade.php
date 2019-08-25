8fad20f089aa2f5124936b909ec7507542a68ad4
Kevin Giovanni <kevin.giovanni@tokopedia.com>
2019-08-25 09:27:16 +0700

    bug fixing

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,19 +2,17 @@
 <html lang="en">
     <head>
         <title>@yield('page-title')</title>
-        <link rel="icon" href="{{ asset('favicon.png', true) }}" type="image/gif" sizes="64x64">
-        <link rel="mask-icon" href="{{ asset('favicon.png', true) }}" color="#FFFFFF">
+        <link rel="icon" href="{{ asset('favicon.png') }}" type="image/gif" sizes="64x64">
         <meta name="theme-color" content="rgba(0,0,0,0.5)">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="description" content="Demo project">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         @if(Request::is('article/*') || Request::is('event/*') || Request::is('warta-umum/*') || Request::is('warta-jemaat/*') || Request::is('search/*'))
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/article.css', true) }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/article.css') }}">
         @else
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css', true) }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
         @endif
     </head>
     <body>
@@ -26,9 +24,9 @@
     </div>
 
     @if(Request::is('article/*') || Request::is('event/*') || Request::is('warta-umum/*') || Request::is('warta-jemaat/*') || Request::is('search/*'))
-    <script src="{{ asset('js/article.js', true) }}"></script>
+    <script src="{{ asset('js/article.js') }}"></script>
     @else
-    <script src="{{ asset('js/app.js', true) }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     @endif
     </body>
 </html>
